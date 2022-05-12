56982092e9ecea1e87c9130a0df5b189407a6a9e
Boscotron <boscotron@users.noreply.github.com>
2022-05-12 16:09:33 -0600

    Update resume_top.ex

--- lib/kajooly_template_beta_web/live/layout_live/resume_top.ex
+++ lib/kajooly_template_beta_web/live/layout_live/resume_top.ex
@@ -15,19 +15,52 @@ defmodule KajoolyTemplateBetaWeb.LayoutLive.ResumeTop do
   import KajoolyTemplateBetaWeb.GenericLive.Text
 
 @doc """
-
+    %{
+      txt_bc_title: "Identidades",
+      txt_bc_title_2: "Identidad",
+      txt_bc_title_3: "Resumen ",
+      txt_bc_title_4: "d.alias",
+      txt_resource_name: "d.alias",
+      to_index: "#",
+      to_this:  "#",
+      txt_count_a: "3",
+      txt_a: "Llaves activas",
+      txt_a_color: "warning",
+      txt_count_b: "3",
+      txt_b: "Accesos activos",
+      txt_b_color: "success",
+    }
   ## Examples
 
-      <.resume_top_two_elemnts
-      color="alert"
-
-      >
-      </.resume_top_two_elemnts>
+    <.resume_top_two_elemnts
+      {%{
+        txt_bc_title: "Identidades",
+        txt_bc_title_2: "Identidad",
+        txt_bc_title_3: "Resumen ",
+        txt_bc_title_4: "d.alias",
+        txt_resource_name: "d.alias",
+        to_index: "#",
+        to_this:  "#",
+        txt_count_a: "3",
+        txt_a: "Llaves activas",
+        txt_a_color: "warning",
+        txt_count_b: "3",
+        txt_b: "Accesos activos",
+        txt_b_color: "success",
+      }}
+    >
+      <:icon_a>
+        <.icons_person_rolodex class="flex-2 mt-2 ms-2 me-3" width="64" height="64" />
+      </:icon_a>
+      <:icon_b>
+        <.icons_check_circle class="flex-2 mt-2 mxms-2 -e " width="64" height="64" />
+      </:icon_b>
+    </.resume_top_two_elemnts>
 
   """
   def resume_top_two_elemnts(assigns) do
     ~H"""
-      <.breadcrum to={assigns[:to_index]} >
+      <.breadcrum to={assigns[:to_index]||"#to_index"} >
         <%= assigns[:txt_bc_title] %>
         <:page to={assigns[:to_index]}> <%= assigns[:txt_bc_title_2] %> </:page>
         <:page to={assigns[:to_this]}> <%= assigns[:txt_bc_title_3] %> </:page>
@@ -51,26 +84,26 @@ defmodule KajoolyTemplateBetaWeb.LayoutLive.ResumeTop do
               </div>
             </div>
             <div class="col-6 col-md-3  ">
-              <div class="card rounded-3 border-primary text-primary">
+              <div class={"card rounded-3 border-#{assigns[:txt_a_color] || "primary" } text-#{assigns[:txt_a_color] || "primary" }"}>
                 <div class="card-body p-1 d-flex ps-2">
-                  <.icons_person_rolodex class="flex-2 mt-2 ms-2 me-3" width="64" height="64" />
+                  <%= render_slot(@icon_a) %>
                   <div class="flex-1">
-                    <h3 class="m-0 text-primary text-center"><%= assigns[:txt_count_roles] %> </h3>
+                    <h3 class={"m-0 text-#{assigns[:txt_a_color] || "primary" } text-center"}><%= assigns[:txt_count_a] || "txt_count_a"  %> </h3>
                     <p class="m-0 text-muted fs-6 text-end">
-                      Roles disponibles
+                    <%= assigns[:txt_a] || "txt_a" %>
                     </p>
                   </div>
                 </div>
               </div>
             </div>
             <div class="col-6 col-md-3  ">
-              <div class="card rounded-3 border-warning text-warning">
+              <div class={"card rounded-3 border-#{assigns[:txt_b_color] || "primary" } text-#{assigns[:txt_b_color] || "primary" }"}>
                 <div class="card-body p-1 d-flex ps-2">
-                  <.icons_check_circle class="flex-2 mt-2 mxms-2 -e " width="64" height="64" />
+                  <%= render_slot(@icon_b) %>
                   <div class="flex-1">
-                    <h3 class="m-0 text-warning text-center"><%= assigns[:txt_count_activity] %> </h3>
+                    <h3 class={"m-0 text-#{assigns[:txt_b_color] || "primary" } text-center"}><%= assigns[:txt_count_b] || "txt_count_b" %> </h3>
                     <p class="m-0 text-muted fs-6 text-end">
-                      Acciones disponibles
+                      <%= assigns[:txt_b] || "txt_b" %>
                     </p>
                   </div>
                 </div>
